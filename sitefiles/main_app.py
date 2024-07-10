eba1480bf5558372c927efccc66f52b7d9872d61
Marcos-Belmont <130399425+Marcos-Belmont@users.noreply.github.com>
2024-07-10 00:15:23 -0300

    Implementando um pouco do front
    50% finalizado o front base.

---
+++ sitefiles/main_app.py
@@ -0,0 +1,25 @@
+# Abre o aplicativo na porta 5500 e o roda.
+from flask import Flask, render_template # type: ignore
+
+app = Flask(__name__) #Recebe a aplicação
+
+#Rotas
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login.html')
+def rota_usuario():
+    return render_template('login.html')
+
+@app.route('/videos.html')
+def rota_video():
+    return render_template('videos.html')
+
+@app.route('/editar_videos.html')
+def rota_editar_video():
+    return render_template('editar_videos.html')
+
+#Inicializa o projeto
+if __name__ == '__main__':
+    app.run(debug = True, port = 5000) 
